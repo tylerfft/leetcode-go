35f2ebc4d16bce45fe30c68546805598b60a0978
tyler <12198276+tyler-fft@users.noreply.github.com>
2024-03-28 14:18:14 +0800

    fix

--- main/600-700/605.go
+++ main/600-700/605.go
@@ -7,8 +7,8 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
 	flowerbed = append(append([]int{0}, flowerbed...), 0)
 	for i := 1; i < len(flowerbed)-1; i++ {
 		if flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 {
+			flowerbed[i] = 1
 			n--
-			i++
 		}
 	}
 	return n <= 0
